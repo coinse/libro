9a008a62c766a6c70642a2053fd16d67c6e59447
Sungmin Kang <stuatlittle@kaist.ac.kr>
2023-01-27 12:51:55 +0900

    removes env.list references

--- run_docker_container.sh
+++ run_docker_container.sh
@@ -3,5 +3,5 @@
 containername="libro-exp"
 
 docker ps -aq --filter "name=$containername" | grep -q . && docker stop $containername && docker rm $containername
-docker run -dt --name $containername --env-file ./env.list -v $(pwd)/data:/root/data -v $(pwd)/scripts:/root/workspace -v $(pwd)/results:/root/results greenmon/libro-env:latest 
+docker run -dt --name $containername -v $(pwd)/data:/root/data -v $(pwd)/scripts:/root/workspace -v $(pwd)/results:/root/results greenmon/libro-env:latest 
 docker exec -it $containername /bin/bash
