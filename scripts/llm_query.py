1fb966cdffbe1dd9a1195a2621544e5e074b61d5
Juyeon Yoon <greenmon@kaist.ac.kr>
2023-03-23 17:13:28 +0900

    Add interface for gpt3.5

--- scripts/llm_query.py
+++ scripts/llm_query.py
@@ -2,6 +2,7 @@
 # coding: utf-8
 
 import os
+import re
 import json
 import openai
 from bs4 import BeautifulSoup
@@ -19,26 +20,68 @@ def query_by_prompt(prompt, end_string_list, mode):
     if mode == 'NL':
         engine = 'text-davinci-002'
     elif mode == 'PL':
-        engine = 'code-davinci-002'
+        engine = 'code-davinci-002' # deprecated
+    elif mode == 'PL_chat':
+        engine = 'gpt-3.5-turbo'
 
-    response = openai.Completion.create(
-        engine=engine,
-        prompt=prompt,
-        temperature=0.7,
-        max_tokens=256,
-        top_p=1,
-        frequency_penalty=0,
-        presence_penalty=0,
-        stop=end_string_list
-    )
-    gen_test = response['choices'][0]['text']
-    assert len(end_string_list) == 1
-    if end_string_list[0] in gen_test:
+    if engine in ['code-davinci-002', 'text-davinci-002']:
+        response = openai.Completion.create(
+            engine=engine,
+            prompt=prompt,
+            temperature=0.7,
+            max_tokens=256,
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=0,
+            stop=end_string_list
+        )
+        gen_test = response['choices'][0]['text']
+
+    elif engine == 'gpt-3.5-turbo':
+        response = openai.ChatCompletion.create(
+            model=engine,
+            messages=prompt, # chat-style prompt
+            n=1,
+            frequency_penalty=0,
+            presence_penalty=0,
+            stop=end_string_list
+        )
+        gen_test = response["choices"][0]["message"]["content"]
+
+    if end_string_list is not None and len(end_string_list) > 0 and end_string_list[0] in gen_test:
         actual_test = gen_test.split(end_string_list[0])[0]
     else:
         actual_test = gen_test
     return actual_test
 
+def make_messages_from_file(rep_title, rep_content,
+                          template_file=TEMPLATE_DIR+'2example_chat.json'):
+    rep_title = BeautifulSoup(rep_title.strip(), 'html.parser').get_text()
+    rep_content = md(rep_content.strip())
+
+    with open(template_file) as f:
+        messages = json.load(f)
+
+        for msg in messages:
+            example_text_path = re.findall(r'{%(.+?)%}', msg['content'])
+            if len(example_text_path) > 0:
+                for ef in example_text_path:
+
+                    with open(os.path.join(TEMPLATE_DIR, ef)) as f:
+                        example_text = f.read()
+                    msg['content'] = msg['content'].replace('{%'+ef+'%}', example_text)
+
+        current_query = messages[-1]['content']
+        bug_report_content = f"""
+        # {rep_title}
+        ## Description
+        {rep_content}
+        """
+        current_query = current_query.replace('{{bug_report_content}}', bug_report_content)
+
+        messages[-1]['content'] = current_query
+
+    return messages, None
 
 def make_prompt_from_file(rep_title, rep_content,
                           use_plain_text, use_html,
@@ -66,11 +109,15 @@ def make_prompt_from_file(rep_title, rep_content,
     return prompt, [end_string]
 
 
-def query_llm_for_gentest(proj, bug_id, template, use_plain_text=False, use_html=False, mode='PL'):
+def query_llm_for_gentest(proj, bug_id, template, use_plain_text=False, use_html=False, mode='PL_chat', save_prompt=False, prompt_save_path=None):
     with open(BR_DIR + proj + '-' + str(bug_id) + '.json') as f:
         br = json.load(f)
 
-    if 'description_fixed' in br:  # handle exceptional cases in 2022-jsoup
+    if mode == 'PL_chat':
+        prompt, stop = make_messages_from_file(
+            br['title'], br['description'],
+            template_file=TEMPLATE_DIR+f'{template}.json')
+    elif 'description_fixed' in br:  # handle exceptional cases in 2022-jsoup
         prompt, stop = make_prompt_from_file(
             br['title'], br['description_fixed'],
             use_plain_text=False,
@@ -81,8 +128,21 @@ def query_llm_for_gentest(proj, bug_id, template, use_plain_text=False, use_html
             use_plain_text=use_plain_text,
             use_html=use_html, template_file=TEMPLATE_DIR+f'{template}.txt')
 
+    if save_prompt:
+        ext = 'json' if mode == 'PL_chat' else 'txt'
+        if prompt_save_path is None:
+            prompt_save_path = f'prompts/{proj}-{bug_id}-{template}.{ext}'
+        with open(prompt_save_path, 'w') as f:
+            if ext == 'json':
+                json.dump(prompt, f, indent=2)
+            else:
+                f.write(prompt)
+
     query_result = query_by_prompt(prompt, stop, mode)
-    gen_test = 'public void test' + query_result
+    if mode != 'PL_chat':
+        gen_test = 'public void test' + query_result
+    else:
+        gen_test = query_result
     return gen_test
 
 
@@ -93,15 +153,17 @@ if __name__ == '__main__':
     parser.add_argument('-b', '--bug_id', type=int, default=23)
     parser.add_argument('--use_html', action='store_true')
     parser.add_argument('--use_plain_text', action='store_true')
-    parser.add_argument('--template', default='2example')
-    parser.add_argument('--mode', default='PL')
+    parser.add_argument('--save_prompt', action='store_true')
+    parser.add_argument('--template', default='2example_chat')
+    parser.add_argument('--mode', default='PL_chat')
     parser.add_argument('-o', '--out', default='output.txt')
+    parser.add_argument('--prompt_out', default=None)
     args = parser.parse_args()
 
     if args.dataset == 'ghrb':
         BR_DIR = llm_exp_config['bug_report_dir']['ghrb']
 
-    gen_test = query_llm_for_gentest(args.project, args.bug_id, args.template, use_plain_text=args.use_plain_text, use_html=args.use_html, mode=args.mode)
+    gen_test = query_llm_for_gentest(args.project, args.bug_id, args.template, use_plain_text=args.use_plain_text, use_html=args.use_html, mode=args.mode, save_prompt=args.save_prompt, prompt_save_path=args.prompt_out)
 
     with open(args.out, 'w') as f:
         f.write(gen_test)
