db519d0f821b62971e186b7a65de545ac8b4a1f1
Juyeon Yoon <greenmon@kaist.ac.kr>
2023-05-26 17:13:10 +0900

    Handle Parameterized test class for injection
    - See also: https://www.tutorialspoint.com/junit/junit_parameterized_test.htm

--- scripts/common.py
+++ scripts/common.py
@@ -51,7 +51,7 @@ def inject_test(repo_path, src_dir, test_dir, gen_test, needed_elements, dry=Fal
 
     return test_name
 
-def is_abstract_test_class(file_content, filepath, titular_class_name):
+def is_injectable_test_class(file_content, filepath, titular_class_name):
     tree = javalang.parse.parse(file_content)
     titular_class_def = None
     for path, node in tree.filter(javalang.tree.ClassDeclaration):
@@ -61,9 +61,15 @@ def is_abstract_test_class(file_content, filepath, titular_class_name):
     
     assert titular_class_def is not None, f'Could not find titular class {titular_class_name} in {filepath}'
     if 'abstract' in titular_class_def.modifiers:
-        return True
+        return False
+
+    for annotation in titular_class_def.annotations:
+        if hasattr(annotation, 'element'):
+            anno = annotation.element
+            if hasattr(anno, 'type') and anno.type.name == 'Parameterized':
+                return False
     
-    return False
+    return True
 
 def get_best_test_class_for_injection(repo_path, test_dir, gen_test):
     # experimental similarity checker
@@ -75,8 +81,8 @@ def get_best_test_class_for_injection(repo_path, test_dir, gen_test):
             filepath = path.join(root, name)
             with codecs.open(filepath, 'r', encoding='utf-8', errors='ignore') as f:
                 file_cont = f.read()
-                if 'abstract' in file_cont:
-                    if is_abstract_test_class(file_cont, filepath, name.removesuffix('.java')):
+                if 'abstract' in file_cont or '@RunWith(Parameterized.class)' in file_cont: 
+                    if not is_injectable_test_class(file_cont, filepath, name.removesuffix('.java')):
                         continue
                 if '@Ignore' in file_cont:
                     continue  # these files are ignored when testing
